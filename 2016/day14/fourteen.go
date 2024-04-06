96458c1ce62d33ce3f7f0dc8a383072ce4a3e3ea
Tiago Barros <tiagobarros99@ua.pt>
2024-04-06 13:03:30 +0100

    refactor day14, it was fugly

--- 2016/day14/fourteen.go
+++ 2016/day14/fourteen.go
@@ -10,12 +10,11 @@ import (
 
 func main() {
 	puzzle := "ahsbgdzn"
-	fmt.Println(puzzle)
-	partOne(puzzle)
-	partTwo(puzzle)
+	fmt.Println("Puzzle:", puzzle)
+	findHashes(puzzle, 1)
 }
 
-func partOne(puzzle string) {
+func findHashes(puzzle string, part int) {
 	found := 0
 	hashes := make([]string, 0)
 
@@ -23,16 +22,27 @@ func partOne(puzzle string) {
 	// we need go over the next 1000 indexes and check if there's a 5 letter sequence of the same triplet (XXXXX)
 	// This just help keep the 1000 hashes in memory for better performance.
 	for i := 1; i < 1000; i++ {
-		h := createHash(puzzle, i)
-		hashes = append(hashes, h)
+		if part == 1 {
+			h := createHash(puzzle, i)
+			hashes = append(hashes, h)
+		} else {
+			h := createNthHash(puzzle, i, 2016)
+			hashes = append(hashes, h)
+		}
 	}
 
 	idx := 1
 	for found < 64 {
-		// We've already created the 1000 first hashes, now create the N+1000
 		currentHash := hashes[0]
-		h := createHash(puzzle, idx+1000)
-		hashes = append(hashes, h)
+		if part == 1 {
+			// We've already created the 1000 first hashes, now create the N+1000
+			h := createHash(puzzle, idx+1000)
+			hashes = append(hashes, h)
+		} else {
+			// We've already created the 1000 first hashes, now create the N+1000
+			h := createNthHash(puzzle, idx+1000, 2016)
+			hashes = append(hashes, h)
+		}
 		// Discard first element
 		hashes = hashes[1:]
 
@@ -48,45 +58,7 @@ func partOne(puzzle string) {
 		}
 		idx++
 	}
-	fmt.Println("Part1: The index of the 64th valid hash was:", idx)
-
-}
-
-func partTwo(puzzle string) {
-	found := 0
-	hashes := make([]string, 0)
-
-	// Create a list of 1000 ashes, because if we do find a triplet in any given hash index
-	// we need go over the next 1000 indexes and check if there's a 5 letter sequence of the same triplet (XXXXX)
-	// This just help keep the 1000 hashes in memory for better performance.
-	for i := 1; i < 1000; i++ {
-		h := createNthHash(puzzle, i, 2016)
-		hashes = append(hashes, h)
-	}
-
-	idx := 1
-	for found < 64 {
-		// We've already created the 1000 first hashes, now create the N+1000
-		currentHash := hashes[0]
-		h := createNthHash(puzzle, idx+1000, 2016)
-		hashes = append(hashes, h)
-		// Discard first element
-		hashes = hashes[1:]
-
-		// If this hash has a triplet, check if it has a 5 repeating of that char in the next 1000 hashes
-		if char := hasTriplet(currentHash); char != "" {
-			// Check if the hashes array contain any has with this char repeating 5 times
-			if slices.ContainsFunc(hashes, func(str string) bool {
-				return strings.Contains(str, strings.Repeat(char, 5))
-			}) {
-				// If both checks pass, we've found a valid hash
-				found++
-			}
-		}
-		idx++
-	}
-	fmt.Println("Part2: The index of the 64th valid hash was:", idx)
-
+	fmt.Printf("Part: %d | The index of the 64th valid hash was: %d\n", part, idx)
 }
 
 // Check if a given string has a triplet, and return the char of said triplet
