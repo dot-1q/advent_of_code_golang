1682c458603dc6f7f59cc41f19a5811f087217a6
Tiago Barros <tiagobarros99@ua.pt>
2024-04-27 14:46:08 +0100

    day 25 added, easy one. day 11 TAKEN FROM OTHER REPO I GIVE IT UP

--- 2016/day25/twenty-five.go
+++ 2016/day25/twenty-five.go
@@ -2,50 +2,70 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
 	f, _ := os.ReadFile("input.txt")
 	lines := string(f)
 	instructions := strings.Split(lines, "\n")
-	part := 2
 
+	// We need 3 values for part 2
+	for i := range math.MaxInt32 {
+		// Means it timedout
+		if findLowestA(instructions, i) {
+			fmt.Printf("Value %d is a valid value\n", i)
+			return
+		}
+	}
+}
+
+func findLowestA(instructions []string, registerA int) bool {
 	// Registers
 	registers := map[string]int{}
-	if part == 1 {
-		registers["a"] = 7
-	} else {
-		registers["a"] = 12
-	}
+	registers["a"] = registerA
 	registers["b"] = 0
 	registers["c"] = 0
 	registers["d"] = 0
 
+	start := time.Now()
 	// PC will be seen as the program counter. it jumps to whatever instructions it needs to jump
 	pc := 0
+	lastValue := -1
 	for pc < len(instructions) {
-		pc += decodeInst(instructions, pc, registers)
-	}
+		// This last value is the return of register b, which should be 0,1,0,1,0,1
+		// Instructions that do not output nothing, return the value -1
+		address, ret := decodeInst(instructions, pc, registers)
+		// If its the out instruction
+		if ret != -1 {
+			// Not alternating
+			if ret == lastValue {
+				// Stop this iteration
+				return false
+			}
+			lastValue = ret
+			current := time.Since(start)
+			// We've been alternating for 5 seconds. We can safely guess this value for register A is valid.
+			if current.Seconds() > 5 {
+				return true
+			}
 
-	fmt.Println(registers)
+		}
+		pc += address
+	}
+	return false
 }
 
 // From the instruction, decode the next jump address in the array of instructions
 // By default return 1, meaning, go to the intnext immediate instruction
-func decodeInst(instructions []string, address int, registers map[string]int) int {
+func decodeInst(instructions []string, address int, registers map[string]int) (int, int) {
 	i := strings.Split(instructions[address], " ")
 	oper := i[0]
 
-	// Hard coded multiplication skippers
-	if instructions[address] == "jnz d -5" && instructions[address-1] == "dec d" &&
-		instructions[address-2] == "jnz c -2" {
-		registers["a"] += registers["b"] * registers["d"]
-		registers["c"] = 0
-		registers["d"] = 0
-	}
 	switch oper {
 	case "cpy":
 		// Check if the second operator is a register. If not, means its an int
@@ -69,38 +89,19 @@ func decodeInst(instructions []string, address int, registers map[string]int) in
 		}
 		// Check if the value of the register is *NOT* zero
 		if val, ok := registers[i[1]]; ok && val != 0 {
-			return jmp
+			return jmp, -1
 		} else {
 			// Previous failed, means first operand is a number
 			val, _ := strconv.Atoi(i[1])
 			if val != 0 {
-				return jmp
+				return jmp, -1
 			}
 		}
-	case "tgl":
-		jump := registers[i[1]]
-		// The jump is outside the program.
-		if (address + jump) > len(instructions) {
-			return 1
-		}
-		toggleInst := strings.Split(instructions[address+jump], " ")
-		oper := toggleInst[0]
-		// Alter the toggle Instruction operation
-		if len(toggleInst) == 2 {
-			if oper == "inc" {
-				toggleInst[0] = "dec"
-			} else {
-				toggleInst[0] = "inc"
-			}
-		} else if len(toggleInst) == 3 {
-			if oper == "jnz" {
-				toggleInst[0] = "cpy"
-			} else {
-				toggleInst[0] = "jnz"
-			}
-		}
-		// Place the toggled instruction in the original instruction array
-		instructions[address+jump] = strings.Join(toggleInst, " ")
+	case "out":
+		value := registers[i[1]]
+		// return the value on register
+		return 1, value
+		// fmt.Println(value)
 	}
-	return 1
+	return 1, -1
 }
